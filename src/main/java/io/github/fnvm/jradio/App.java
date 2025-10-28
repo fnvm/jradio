6a7d0fbd57589b9b18f42912b6711fc34286bcfa
Vladimir <test@test.com>
2025-10-28 19:07:36 +0300

    logging settings updated

--- src/main/java/io/github/fnvm/jradio/App.java
+++ src/main/java/io/github/fnvm/jradio/App.java
@@ -1,11 +1,8 @@
 package io.github.fnvm.jradio;
 
 import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 import io.github.fnvm.jradio.ui.menu.MainMenu;
 import io.github.fnvm.jradio.ui.terminal.TerminalManager;
@@ -20,25 +17,9 @@ public class App {
 			e.printStackTrace();
 		}
 	}
-	
-    public static void setupLogging() {
-        try {
-            Logger rootLogger = Logger.getLogger("");
 
-            Handler[] handlers = rootLogger.getHandlers();
-            for (Handler handler : handlers) {
-                rootLogger.removeHandler(handler);
-            }
-
-            FileHandler fileHandler = new FileHandler("app.log", true);
-            fileHandler.setFormatter(new SimpleFormatter());
-
-            rootLogger.addHandler(fileHandler);
-
-            rootLogger.setLevel(Level.SEVERE);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void setupLogging() {
+		Logger rootLogger = Logger.getLogger("");
+		rootLogger.setLevel(Level.OFF);
+	}
 }
